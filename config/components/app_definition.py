97b31c23a076847e6f9f232d7596523fe93a6ca3
fernando <terrazasllanosfernando@gmail.com>
2026-04-27 00:00:27 -0400

    ejercicio terminado, faltan aumentar detalles al proyecto

--- config/components/app_definition.py
+++ config/components/app_definition.py
@@ -27,3 +27,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOCALE_PATHS = ['movies/locale']
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
